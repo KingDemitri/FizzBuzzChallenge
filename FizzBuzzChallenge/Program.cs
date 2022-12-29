bdfb7bd27befde011c9fca1ede977e94eda78471
Demitri Cameron <oblivion9794@gmail.com>
2022-12-29 12:48:20 -0500

    initial commit

---
+++ FizzBuzzChallenge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System;
+using System.Runtime.InteropServices;
+
+namespace LINQ
+{
+    public class FizzBuzzChallenge
+    {
+
+        public static string FizzBuzz(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "fizzbuzz";
+            }
+            else if (num % 3 == 0)
+            {
+                return "fizz";
+            }
+            else if (num % 5 == 0)
+            {
+                return "buzz";
+            }
+            else return "";
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzBuzz(3)); 
+            Console.WriteLine(FizzBuzz(5)); 
+            Console.WriteLine(FizzBuzz(15)); 
+            Console.WriteLine(FizzBuzz(1));  
+        }
+    }
+}
